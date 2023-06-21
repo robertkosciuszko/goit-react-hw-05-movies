446fbefef6f64d74d98673acf8bcc2167d67ce7a
robertkosciuszko <114183493+robertkosciuszko@users.noreply.github.com>
2023-06-21 14:20:20 +0200

    uppdate

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { useTrendingList } from '../utils/hooks/useTrendingList';
+import { TrendingList } from 'components/TrendingList/TrendingList';
+
+const HomePage = () => {
+  const { trendingMovies } = useTrendingList();
+  return (
+    <div>
+      <TrendingList movies={trendingMovies} />
+    </div>
+  );
+};
+export default HomePage;
